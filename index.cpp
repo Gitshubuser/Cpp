217d57429c30003eae9b997214e22adb93c7bc3d
Ritik <ritikrathore336699@gmail.com>
2022-04-20 00:51:46 +0530

    initial commit

---
+++ index.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std ;
+
+int main()
+{
+    system("cls");
+    
+    int a=1;
+    // assign 5 to a to amke it error free
+    if(a!=5)
+    {
+    cout<<"File has an Error\n";
+    }
+    else
+    {
+        cout<<"File is OK \n";
+    }
+    return 0;
+}
